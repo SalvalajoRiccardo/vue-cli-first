26d6adeec2fe1e86ee726894484076e1fe645e4a
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-07-05 16:22:11 +0200

    eroore bonus

---
+++ vuecli1/src/components/Hero.vue
@@ -0,0 +1,18 @@
+<template>
+  <img src="../assets/logo.png" alt="a">
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped >
+img {
+  margin: 40px 0 0;
+  width: 50%;
+}
+
+</style>
